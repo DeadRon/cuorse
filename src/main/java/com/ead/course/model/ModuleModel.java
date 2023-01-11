222084bbe7f9e08d935b7753c9e59a8b529cd244
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-11 02:09:28 -0300

    relação ManyToOne entre Module/Course e relação OneToMany entre Module/Lesson

--- src/main/java/com/ead/course/model/ModuleModel.java
+++ src/main/java/com/ead/course/model/ModuleModel.java
@@ -2,10 +2,12 @@ package com.ead.course.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -26,4 +28,10 @@ public class ModuleModel {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
+    @ManyToOne(optional = false)
+    private CourseModel course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//não permite a serialização
+    @OneToMany(mappedBy = "module")
+    private Set<LessonModel> lessons;
 }
