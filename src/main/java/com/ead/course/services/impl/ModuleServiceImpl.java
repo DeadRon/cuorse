1a4e604fefdcaf67de08c0e79ca4b295e5998642
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-16 23:44:35 -0300

    exclusao de Module e Lessos associados a ele

--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,14 +1,32 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.model.LessonModel;
+import com.ead.course.model.ModuleModel;
+import com.ead.course.repository.LessonRepository;
 import com.ead.course.repository.ModuleRepository;
-import com.ead.course.services.CourseService;
+import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
-public class ModuleServiceImpl implements CourseService {
+public class ModuleServiceImpl implements ModuleService {
 
     @Autowired
     ModuleRepository moduleRepository;
 
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel module) {
+        List<LessonModel> lessonModels = lessonRepository.findAllLessonsIntoCourse(module.getModuleId());
+        if(!lessonModels.isEmpty()){
+            lessonRepository.deleteAll(lessonModels);
+        }
+        moduleRepository.delete(module);
+    }
 }
