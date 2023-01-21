c6f16acb20264832bd7d3505893daf3d617d2d5d
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-21 16:22:41 -0300

    Novos métodos para salvar, buscar por id e buscar todos

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -2,8 +2,17 @@ package com.ead.course.services;
 
 import com.ead.course.model.CourseModel;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CourseService {
 
     void delete(CourseModel courseModel);
 
+    CourseModel save(CourseModel cuorseModel);
+
+    Optional<CourseModel> findBy(UUID cuorseId);
+
+    List<CourseModel> findAll();
 }
