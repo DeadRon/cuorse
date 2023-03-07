5a2bd62db8e87276c0e32db15375e0e823a3eeff
Ronaldo Melo <ronaldorpm1994@gmail.com>
2023-03-07 02:52:53 -0300

    specificantion template para Lesson

--- src/main/java/com/ead/course/controllers/LessonController.java
+++ src/main/java/com/ead/course/controllers/LessonController.java
@@ -5,8 +5,13 @@ import com.ead.course.model.LessonModel;
 import com.ead.course.model.ModuleModel;
 import com.ead.course.services.LessonService;
 import com.ead.course.services.ModuleService;
+import com.ead.course.specification.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,8 @@ import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static com.ead.course.specification.SpecificationTemplate.moduleCourseId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -78,8 +85,11 @@ public class LessonController {
     }
 
     @GetMapping("/modules/{moduleId}/lessons")
-    public ResponseEntity<List<LessonModel>> findAllLessonsByModule(@PathVariable("moduleId")UUID moduleId){
-        List<LessonModel> lessonModels = lessonService.findAllLessonsIntoModule(moduleId);
+    public ResponseEntity<Page<LessonModel>> findAllLessonsByModule(@PathVariable("moduleId")UUID moduleId,
+                                                                    SpecificationTemplate.LessonSpec spec,
+                                                                    @PageableDefault(page = 0, size = 10, sort = "lessonId", direction = Sort.Direction.ASC) Pageable pageable
+                                                                    ){
+        Page<LessonModel> lessonModels = lessonService.findAllLessonsIntoModule(SpecificationTemplate.lessonModuleId(moduleId).and(spec), pageable);
         return ResponseEntity.status(HttpStatus.OK).body(lessonModels);
     }
 
