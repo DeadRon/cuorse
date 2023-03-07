52a894f6a686e54826d925129c43ebbe2f3c1ef4
Ronaldo Melo <ronaldorpm1994@gmail.com>
2023-03-07 02:53:30 -0300

    Specification template para Lesson e Module

--- src/main/java/com/ead/course/specification/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specification/SpecificationTemplate.java
@@ -1,6 +1,7 @@
 package com.ead.course.specification;
 
 import com.ead.course.model.CourseModel;
+import com.ead.course.model.LessonModel;
 import com.ead.course.model.ModuleModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
@@ -23,13 +24,29 @@ public class SpecificationTemplate {
     })
     public interface CourseSpec extends Specification<CourseModel> {}
 
+    @And(@Spec(path = "title", spec = Like.class))
+    public interface ModuleSpec extends Specification<ModuleModel> {}
+
+    @And(@Spec(path = "title", spec = Like.class))
+    public interface LessonSpec extends Specification<LessonModel> {}
+
     public static Specification<ModuleModel> moduleCourseId(final UUID courseId){
         return (root, query, cb) -> {
             query.distinct(true);
             Root<ModuleModel> module = root;
             Root<CourseModel> course = query.from(CourseModel.class);
             Expression<Collection<ModuleModel>> coursesModules = course.get("modules");
-            return cb.and(cb.equal(course.get("modules"), courseId), cb.isMember(module, coursesModules));
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, coursesModules));
+        };
+    }
+
+    public static Specification<LessonModel> lessonModuleId(final UUID moduleId){
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<LessonModel> lesson = root;
+            Root<ModuleModel> course = query.from(ModuleModel.class);
+            Expression<Collection<LessonModel>> moduleLessons = course.get("lessons");
+            return cb.and(cb.equal(course.get("courseId"), moduleId), cb.isMember(lesson, moduleLessons));
         };
     }
 
